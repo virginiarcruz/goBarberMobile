d3ea901a3a4486d63394816239e70bdd351d3cdb
Virginia Rodrigues Cruz <virginia.cruz@ifactory.com.br>
2020-02-04 15:46:54 -0300

    conect redux signin and signup pages

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,9 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Background from '../../components/Background';
+import { signInRequest } from '~/store/modules/auth/actions';
 import logo from '~/assets/logo.png';
 
 import {
@@ -14,9 +16,17 @@ import {
 } from './styles';
 
 export default function SignIn({ navigation }) {
+  const dispath = useDispatch();
   const passwordRef = useRef();
 
-  function handleSubmit() {}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit() {
+    dispath(signInRequest(email, password));
+  }
 
   return (
     <Background>
@@ -32,6 +42,8 @@ export default function SignIn({ navigation }) {
             placeholder="Digite seu e-mail"
             returnKeyType="next"
             onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
           />
           <FormInput
             icon="lock-outline"
@@ -40,9 +52,13 @@ export default function SignIn({ navigation }) {
             ref={passwordRef}
             returnKeyType="send"
             onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
           />
 
-          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+          <SubmitButton loading={loading} onPress={handleSubmit}>
+            Acessar
+          </SubmitButton>
         </Form>
         <SignLink onPress={() => navigation.navigate('SignUp')}>
           <SignLinkText> Criar conta gratuita</SignLinkText>
