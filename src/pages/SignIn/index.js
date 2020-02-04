c868052bf685e5f9f295d4334c068c52393ff877
Virginia Rodrigues Cruz <virginia.cruz@ifactory.com.br>
2020-02-03 23:43:44 -0300

    add buttons icons and input

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 import Background from '../../components/Background';
-
-// import { Container } from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text> sign iin </Text>
+      <Text> Sign In </Text>
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+      <Button> Entrar </Button>
     </Background>
   );
 }
